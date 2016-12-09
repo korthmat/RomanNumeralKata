8258f86d46a1e8addc30cdae15168d91e5cfeee6
korthmat <kalten@sandwich.net>
2016-12-09 11:12:43 -0500

    Added remaining conversions (4 -> IV, 9 -> IX, 40 -> XL, 90 -> XC, 400 -> CD, 500 -> D, 900 -> CM, 1000 -> M) to the lookup table.

--- RomanNumeralKata/RomanNumeral.cs
+++ RomanNumeralKata/RomanNumeral.cs
@@ -11,10 +11,18 @@ namespace RomanNumeralKata
         private static readonly Dictionary<int, string> _arabicToRomanLookup = new Dictionary<int, string>()
         {
             { 1, "I" },
+            { 4, "IV" },
             { 5, "V" },
+            { 9, "IX" },
             { 10, "X" },
+            { 40, "XL" },
             { 50, "L" },
-            { 100, "C" }
+            { 90, "XC" },
+            { 100, "C" },
+            { 400, "CD" },
+            { 500, "D" },
+            { 900, "CM" },
+            { 1000, "M" }
         };
 
         public string ToRoman(int arabicNumber)
