f73f7823f468602ee66644424f2e155af1763fbc
korthmat <kalten@sandwich.net>
2016-12-09 12:14:56 -0500

    Added a quick-and-dirty demo program.

--- RomanNumeralKata/Program.cs
+++ RomanNumeralKata/Program.cs
@@ -8,8 +8,37 @@ namespace RomanNumeralKata
 {
     class Program
     {
+        static readonly RomanNumeral _roman = new RomanNumeral();
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Converting Arabic numerals to Roman numerals:");
+            ConvertToRoman(-1);
+            ConvertToRoman(4321);
+            ConvertToRoman(1);
+            ConvertToRoman(3);
+            ConvertToRoman(9);
+            ConvertToRoman(1066);
+            ConvertToRoman(1989);
+            ConvertToRoman(2016);
+            ConvertToRoman(3888);
+
+            Console.WriteLine();
+            Console.WriteLine("Press Return/Enter to continue");
+
+            Console.ReadLine();
+        }
+
+        static void ConvertToRoman(int numberToConvert)
+        {
+            try
+            {
+                Console.WriteLine("{0} => {1}", numberToConvert, _roman.ToRoman(numberToConvert));
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("{0} => conversion failed, number is outside the range of numbers that can be converted", numberToConvert);
+            }
         }
     }
 }
