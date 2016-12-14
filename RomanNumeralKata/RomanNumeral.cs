5d8fb078bc42d6e5c7c1a5db3d5ecbf453157ffd
korthmat <kalten@sandwich.net>
2016-12-14 13:01:17 -0500

    Removed the comment.  Moved _arabicToRomanLookup to outside the ToRoman region.

--- RomanNumeralKata/RomanNumeral.cs
+++ RomanNumeralKata/RomanNumeral.cs
@@ -7,8 +7,6 @@ namespace RomanNumeralKata
 {
     public class RomanNumeral
     {
-        #region ToRoman
-
         private static readonly List<ArabicToRomanConversion> _arabicToRomanLookup = new List<ArabicToRomanConversion>()
         {
             new ArabicToRomanConversion(1, "I"),
@@ -26,6 +24,8 @@ namespace RomanNumeralKata
             new ArabicToRomanConversion(1000, "M")
         };
 
+        #region ToRoman
+
         public string ToRoman(int arabicNumber)
         {
             if (arabicNumber <= 0 || arabicNumber >= 4000)
@@ -36,26 +36,6 @@ namespace RomanNumeralKata
                     "An Arabic number must be between 1 and 3999 to be converted to Roman numerals.");
             }
 
-            /*
-             * Basic strategy here:
-             * - go through the Arabic-to-Roman lookup in reverse order by value (1000 to 1).
-             * - find out how many of that value exists in the Arabic value
-             *      - if 0, move on
-             *      - if > 0,
-             *          - append that many of the appropriate Roman numeral to the output.
-             *          - reduce the value to be converted by the amount you just converted to
-             *              Roman (e.g., if it's 2001 and you just converted 2000 to MM, reduce
-             *              the value to convert by 2000 -> 1)
-             * - when done, return the Roman numeral string
-             * 
-             * This leads to (for example):
-             *  1973
-             *   973    M
-             *    73    MCM
-             *    23    MCML
-             *     3    MCMLXX
-             *     0    MCMLXXIII
-             */
             StringBuilder romanNumeral = new StringBuilder();
             foreach (ArabicToRomanConversion conversion in _arabicToRomanLookup.OrderByDescending(arc => arc.Arabic))
             {
